eaa1b4c95edd8358146be44319c58930d71ce072
Mihai Fufezan <fufexan@protonmail.com>
2023-08-29 10:41:01 +0100

    Rebrand

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" height="300">
+  <HelloWorld msg="Welcome to STS Bootcamp 2023"/>
 </template>
 
 <script>
